bf5b36d11e19fc41bf6c18db2375ec52dc197252
sep <sebastian.lang15@gmail.com>
2024-07-07 20:54:06 +0200

    Add report function

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    print_report("books/frankenstein.txt")
+
+def print_report(path):
+    text = get_book_text(path)
+    print(f"--- Begin report of {path} ---")
     print(count_words(text))
-    print(count_characters(text))
+
+    chars = count_characters(text)
+    sorted_chars = sorted(chars, key=chars.get, reverse=True)
+    for key in sorted_chars:
+        if key.isalpha():
+            print(f"The '{key}' character was found {chars[key]} times")
+
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
